7dd554be099027b06d9068dcd379733a23d8c817
Aranda037 <arandajavier98it@gmail.com>
2019-10-31 00:49:14 -0700

    Validacion x

--- Agiles/app/src/main/java/com/example/proyectomovilagiles/Login.kt
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/Login.kt
@@ -13,7 +13,23 @@ class Login : AppCompatActivity() {
 
         btnLogin.setOnClickListener {
             val intent = Intent(this, MenuMaterias::class.java)
-            startActivity(intent)
+            var id = txtUsuario.text.toString()
+            var pass = txtPass.text.toString()
+            if(validacion(id,pass)){
+                startActivity(intent)
+            }else{
+               incorrecto.text = "*Credenciales invalidas*"
+            }
+
         }
+    }
+
+    fun validacion(id:String, pass:String):Boolean{
+
+        if(id.equals("00000164788") && pass.equals("12345")){
+            return true
+        }
+
+        return false
     }
 }
