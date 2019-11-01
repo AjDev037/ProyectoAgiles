8cb6fd38fdc99fae3f0d2f29666d8f17a8f37355
Diego <32893011+DiegoEvil@users.noreply.github.com>
2019-10-31 17:43:55 -0700

    Cardview para Materias
    -Se agrego la lista para la vista de las materias
    -Se agrego la carta para las materias

--- Agiles/app/src/main/java/com/example/proyectomovilagiles/MenuMaterias.kt
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/MenuMaterias.kt
@@ -1,19 +1,32 @@
 package com.example.proyectomovilagiles
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.activity_menu_materias.*
+import kotlinx.android.synthetic.main.materia_cardview.view.*
+import objetos.Materia
 
 class MenuMaterias : AppCompatActivity() {
 
-
+    var listaMaterias = ArrayList<Materia>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_materias)
 
+        crearMaterias()
+
         val preferencias = MyPreference(this)
+
+        var adaptador = AdaptadorMateria(this, listaMaterias)
+        listview.adapter = adaptador
 
         btnSalir.setOnClickListener {
             preferencias.setPass("")
@@ -21,6 +34,60 @@ class MenuMaterias : AppCompatActivity() {
             val intent = Intent(this,Login::class.java)
             startActivity(intent)
         }
+
+    }
+
+    fun crearMaterias(){
+        var materia1 = Materia("Materia1", "Hoy", "Hora Actual", "1800s", R.drawable.ic_backgroundtest);
+        var materia2 = Materia("Materia2", "Manana", "Hora Actual", "1800s", R.drawable.ic_backgroundtest);
+
+        listaMaterias.add(materia1)
+        listaMaterias.add(materia2)
+    }
+
+    private class AdaptadorMateria:BaseAdapter{
+
+        var contexto: Context? = null
+        var materias = ArrayList<Materia>()
+
+        constructor(context: Context, materias: ArrayList<Materia>){
+            contexto = context
+            this.materias = materias
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var inflador = LayoutInflater.from(contexto)
+            var vista = inflador.inflate(R.layout.materia_cardview, null)
+            var materia = materias[position]
+
+
+            //TODO("Agregar una imagen de fondo para la carta, separada de la materia")
+            vista.card.setBackgroundResource(materia.imagen!!)
+            vista.materia_foto.setImageResource(materia.imagen!!)
+            vista.materia_nombre.text = materia.nombre
+            vista.materia_fecha.text = materia.fecha
+            vista.materia_hora.text = materia.hora
+            vista.materia_salon.text = materia.salon
+
+            vista.setOnClickListener{
+                //TODO("Call next activity")
+            }
+
+            return vista
+        }
+
+        override fun getItem(position: Int): Any {
+            return materias[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return materias.size
+        }
+
 
     }
 }
--- Agiles/app/src/main/java/objetos/Materia.kt
+++ Agiles/app/src/main/java/objetos/Materia.kt
@@ -2,6 +2,6 @@ package objetos
 
 import java.io.Serializable
 
-class Materia(var nombre:String, var fecha:String, var hora:String, var salon:String):Serializable{
- constructor() :this ("" ,"","","")
+class Materia(var nombre:String, var fecha:String, var hora:String, var salon:String, var imagen:Int?):Serializable{
+ constructor() :this ("" ,"","","", null)
 }
