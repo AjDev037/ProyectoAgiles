7ce6b6643e6aa415aecffdb17a21f22fb1e0b751
Aranda037 <arandajavier98it@gmail.com>
2019-11-05 00:18:19 -0700

    Agregue menu materias Profesor
    Lo agregue para no andar batallando con la logica, que se tome con actividades separadas

---
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/MenuMateriasProfesor.kt
@@ -0,0 +1,12 @@
+package com.example.proyectomovilagiles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuMateriasProfesor : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_materias_profesor)
+    }
+}
