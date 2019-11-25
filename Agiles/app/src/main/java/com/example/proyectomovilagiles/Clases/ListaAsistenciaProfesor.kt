df1ec0320aa6d7f4ce15fb63fe807bc7eae8e9de
Diego <32893011+DiegoEvil@users.noreply.github.com>
2019-11-24 18:35:24 -0700

    Update ListaAsistenciaProfesor.kt
    -Se agrego un dialogo para justificar asistencias.

--- Agiles/app/src/main/java/com/example/proyectomovilagiles/Clases/ListaAsistenciaProfesor.kt
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/Clases/ListaAsistenciaProfesor.kt
@@ -1,6 +1,9 @@
 package com.example.proyectomovilagiles.Clases
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -70,8 +73,37 @@ class ListaAsistenciaProfesor : AppCompatActivity() {
             }
 
             vista.setOnClickListener {
-                //AQUI PON EL POP UP DIEGO PLIS
-                Toast.makeText(context,"AAAAAH",Toast.LENGTH_LONG).show()
+
+                val mAlertDialog = AlertDialog.Builder(context)
+                //Le damos un icono
+                mAlertDialog.setIcon(R.mipmap.ic_launcher_round)
+                //Le damos un titulo
+                mAlertDialog.setTitle("Justificacion")
+                //Y le ponemos un mensaje
+                mAlertDialog.setMessage("Desea justificar esta asistencia?")
+
+                //Le damos un listener a la accion de continuar y un texto
+                mAlertDialog.setPositiveButton("Si") { dialog, id ->
+                    //Le sumamos uno al estado
+                    asi.estado += 1
+
+                    //Aqui puedes poner el llamado a la base de datos uwu
+                }
+
+                //Le damos un listener a la accion negativa y un texto
+                mAlertDialog.setNegativeButton("No") { dialog, id ->
+                    //Si dijo que no, no hacemos nada...
+
+                }
+                //Y mostramos el cuadro de dialogo
+
+                if(asi.estado < 1) {
+                    mAlertDialog.show()
+                } else {
+                    Toast.makeText(context,"No puede justificar una asistencia",Toast.LENGTH_LONG).show()
+                }
+
+                Toast.makeText(context,asi.estado.toString(),Toast.LENGTH_LONG).show()
             }
             return vista
 
