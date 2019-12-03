cde8afd6801dd5710de6e5820585945acffe47d5
Aranda037 <arandajavier98it@gmail.com>
2019-12-03 16:12:59 -0700

    ya hay notificaciones

--- Agiles/app/src/main/java/com/example/proyectomovilagiles/Hilo.java
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/Hilo.java
@@ -12,14 +12,14 @@ public class Hilo implements Runnable {
 
     @Override
     public void run() {
-        for(int i= 0; i<= 30;i++){
+        for(int i= 0; i<= 15;i++){
 
             try {
                 System.out.println("ME DORMI");
 
-                Thread.sleep(4000);
+                Thread.sleep(1000);
                 System.out.println("ESTOY IMPRIMIENDO EL NUMERO: " + i);
-                if(i == 30){
+                if(i == 15){
                     obs.notificar("Hilo");
                     System.out.println("YA NO VOY A CONTAR, QUE HUEVA");
                 }
--- Agiles/app/src/main/java/com/example/proyectomovilagiles/MyService.java
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/MyService.java
@@ -1,15 +1,26 @@
 package com.example.proyectomovilagiles;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import org.jetbrains.annotations.NotNull;
 
 import objetos.Observer;
 
 public class MyService extends Service implements Observer {
+
+
+    private final static String CHANNEL_ID = "NOTIFICACION";
+    private final static int NOTIFICACION_ID = 0;
+
     public MyService() {
     }
 
@@ -36,5 +47,32 @@ public class MyService extends Service implements Observer {
     @Override
     public void notificar(@NotNull String name) {
         System.out.println("ME NOTIFICARON");
+        notificacionChannel();
+        crearNotificacion();
+    }
+
+    public void crearNotificacion(){
+        NotificationCompat.Builder notificacion = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        notificacion.setSmallIcon(R.drawable.ic_launcher_foreground);
+        notificacion.setTicker("Notificacion por tiempo");
+        notificacion.setWhen(System.currentTimeMillis());
+        notificacion.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificacion.setContentTitle("Se acabo el tiempo");
+        notificacion.setContentText("APACHURRALE");
+
+        NotificationManagerCompat nm = NotificationManagerCompat.from(getApplicationContext());
+        assert nm != null;
+        nm.notify(NOTIFICACION_ID, notificacion.build());
+    }
+
+    public void notificacionChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Notificacion";
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
+
+
