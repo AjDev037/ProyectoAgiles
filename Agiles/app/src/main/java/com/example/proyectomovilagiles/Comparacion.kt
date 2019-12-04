e38481ebac78ac63b397961cdafa91c0805c9b1a
Aranda037 <arandajavier98it@gmail.com>
2019-12-04 14:16:33 -0700

    Eliminacion de Hilos
    El hilo bueno se llama comparacion.

--- Agiles/app/src/main/java/com/example/proyectomovilagiles/Comparacion.kt
+++ Agiles/app/src/main/java/com/example/proyectomovilagiles/Comparacion.kt
@@ -8,21 +8,31 @@ import objetos.Observer
 
 class Comparacion(var obs:Observer, var db : DbHandler): Runnable{
 
-    val observer :Observer = obs
-    var bd : DbHandler = db
-    var notificado = false;
+    //Revisa si ya se envio la notificacion una vez, ahorita no sirve de nada
+    //pero eso deberia hacer.
+    var notificado = false
 
 
     override fun run() {
         while (true){
-            for( mat in bd.leerDatos()){
+            //Se mete a la bd para leer las materias del usuario.
+            for( mat in db.leerDatos()){
+                //Compara si el dia de hoy es el dia de alguna de las materias.
                 if(mat.dia == getDiaActual()){
+                    //En caso de ser verdadero, se compara la hora para ver si es
+                    //la hora correcta y mandar la notificacion al usuario.
+                    // NOTA: AQUI ES DONDE LA COMPARACION deberia ser de de 15 minutos
+                    // DE DIFERENCIA EN LUGAR DE LA MERA HORA>
                     if(mat.hora == getHoraActual()){
-                            observer.notificar(mat.nombre)
+                            //Si cumple la condicion manda a notificar al observador para que
+                            // envie el mensaje al telefono.
+                            obs.notificar(mat.nombre)
                             notificado = true
                     }
                 }
-                Thread.sleep(3000)
+                //Hago dormir el thread un ratito para que no este siempre en segundo plano
+                //haciendo el run.
+                Thread.sleep(20000)
             }
         }
     }
